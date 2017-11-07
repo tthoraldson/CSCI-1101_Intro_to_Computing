68899c0b95574589493455509d2b9b5b447f8d46
Theresa Thoraldson <theresa.thoraldson@outlook.com>
2017-11-07 13:16:41 -0600

    initial commit

---
+++ main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    printf("Theresa Thoraldson\n\n");
+    printf("CSCI 1101\n");
+    printf("Introduction to Computing and Problem Solving");
+    printf("November 7th 2017");
+}
