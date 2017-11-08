a52cb4acfc4f262c7a94c38c8ab50c3bbe01f98e
Theresa Thoraldson <theresa.thoraldson@outlook.com>
2017-11-08 13:23:11 -0600

    worskpace

--- project1.c
+++ project1.c
@@ -1,3 +1,4 @@
+// C Programming - Project I
 #include <stdio.h>
 
 int main() {
---
+++ project2.c
@@ -0,0 +1,15 @@
+// C Programming - Project II
+#include <stdio.h>
+
+int main() {
+    printf("Theresa Thoraldson\n\n");
+    // Course Number
+    printf("Please Enter the Course Number:\n");
+    char courseNumber = scanf("%s")
+
+    printf("Introduction to Computing and Problem Solving\n");
+    printf("C Project 1\n\n");
+
+    // Comment
+    printf("November 7th 2017\n");
+}
