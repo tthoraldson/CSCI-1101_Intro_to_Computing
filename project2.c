86f9cf5fd424824467b5ce768fa0791561e620ec
Theresa Thoraldson <theresa.thoraldson@outlook.com>
2017-11-10 17:24:12 -0600

    moved p1 to p2

--- project2.c
+++ project2.c
@@ -2,14 +2,27 @@
 #include <stdio.h>
 
 int main() {
-    printf("Theresa Thoraldson\n\n");
-    // Course Number
-    printf("Please Enter the Course Number:\n");
-    char courseNumber = scanf("%s")
+    // variables
+    char name[] = "Theresa Thoraldson";
+    int courseNumber = 1101;
+    char courseTitle[] = "Introduction to Computing and Problem Solving";
+    int projectNumber = 1;
+    char currentDate[] = "November 13th, 2017";
 
-    printf("Introduction to Computing and Problem Solving\n");
-    printf("C Project 1\n\n");
-
-    // Comment
-    printf("November 7th 2017\n");
+    // print statements
+    printf("%s\n\n", name);
+    printf("%i\n", courseNumber);
+    printf("%s\n", courseTitle);
+    printf("C Project %i\n\n", projectNumber);
+    printf("%s\n", currentDate);
 }
+
+// Output:
+
+// Theresa Thoraldson
+//
+// 1101
+// Introduction to Computing and Problem Solving
+// C Project 1
+//
+// November 13th 2017
