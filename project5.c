671b3a6f72f31f054c0dc846c125854d36be0e4f
Theresa Thoraldson <theresa.thoraldson@outlook.com>
2017-12-06 22:56:00 -0600

    create project5.c

---
+++ project5.c
@@ -0,0 +1,25 @@
+// C Programming - Project I
+#include <stdio.h>
+
+int main() {
+    // VARIABLES
+    char name[] = "Theresa Thoraldson";
+    int courseNumber = 1101;
+    char courseTitle[] = "Introduction to Computing and Problem Solving";
+    int projectNumber = 1;
+    char currentDate[] = "December 1st, 2017";
+
+
+
+    // LOGIC
+
+
+
+    // OUTPUT
+    printf("%s\n\n", name);
+    printf("%i\n", courseNumber);
+    printf("%s\n", courseTitle);
+    printf("C Project %i\n\n", projectNumber);
+    printf("%s\n\n", currentDate);
+
+}
