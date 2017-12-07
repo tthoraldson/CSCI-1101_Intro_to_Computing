d3cd09028c334ecda901c1926c22486f97e535b8
Theresa Thoraldson <theresa.thoraldson@outlook.com>
2017-12-06 22:53:32 -0600

    completed project 3

--- project3.c
+++ project3.c
@@ -2,36 +2,41 @@
 #include <stdio.h>
 
 int main() {
-    // variables
+    // VARIABLES
     char name[] = "Theresa Thoraldson";
     int courseNumber = 1101;
     char courseTitle[] = "Introduction to Computing and Problem Solving";
     int projectNumber = 1;
-    char currentDate[] = "November 13th, 2017";
+    char currentDate[] = "November 23rd, 2017";
 
+    int count = 2;
     int inputPhrase;
-    char tempString[4];
-    int number1, number2, number3;
+    int numbers[3];
 
-    // logic
+    // LOGIC
     printf("Enter any Three Digit Number:  ");
     scanf("%d", &inputPhrase);
+    int inputPhraseCopy = inputPhrase;
 
-    printf("%d\n", inputPhrase);
-    number1 = tempPhrase[0];
-    number2 = tempPhrase[1];
-    number3 = tempPhrase[2];
-
-    printf("number1: %c", number1);
-
-//    // OUTPUT
-//    printf("%s\n\n", name);
-//    printf("%i\n", courseNumber);
-//    printf("%s\n", courseTitle);
-//    printf("C Project %i\n\n", projectNumber);
-//    printf("%s\n", currentDate);
+    while(inputPhrase)
+    {
+        numbers[count] = inputPhrase % 10;
+        inputPhrase /= 10;
+        count--;
+    }
 
 
+    // OUTPUT
+    printf("%s\n\n", name);
+    printf("%i\n", courseNumber);
+    printf("%s\n", courseTitle);
+    printf("C Project %i\n\n", projectNumber);
+    printf("%s\n\n", currentDate);
+
+    printf("Original Number: %i\n", inputPhraseCopy);
+    printf("Number 1: %i\n", numbers[0]);
+    printf("Number 2: %i\n", numbers[1]);
+    printf("Number 3: %i\n", numbers[2]);
 }
 
 // Output:
@@ -42,4 +47,9 @@ int main() {
 // Introduction to Computing and Problem Solving
 // C Project 1
 //
-// November 13th 2017
+// November 23rd 2017
+//
+// Original Number: 123
+// Number 1: 1
+// Number 2: 2
+// Number 3: 3
