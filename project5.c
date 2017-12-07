318fe7d34b661dc7f708072dee198ca191b6d149
Theresa Thoraldson <theresa.thoraldson@outlook.com>
2017-12-06 23:39:11 -0600

    project complete

--- project5.c
+++ project5.c
@@ -6,11 +6,15 @@ int main() {
     char name[] = "Theresa Thoraldson";
     int courseNumber = 1101;
     char courseTitle[] = "Introduction to Computing and Problem Solving";
-    int projectNumber = 1;
+    int projectNumber = 5;
     char currentDate[] = "December 1st, 2017";
 
     int inputValue1;
     int inputValue2;
+    int multiples[1000];
+    int multiplesCount = 0;
+    int multiplesTotal = 0;
+
 
     // LOGIC
     printf("Enter value 1: ");
@@ -26,15 +30,33 @@ int main() {
         scanf("%d", &inputValue2);
     };
 
-    printf("Values: %i %i\n\n", inputValue1, inputValue2);
+    // printf("Values: %i %i\n\n", inputValue1, inputValue2);
+
+    // Find multiples of 7
+    for (int i = inputValue1; i <= inputValue2; i++){
+        if (i % 7 == 0){
+            multiples[multiplesCount] = i;
+            multiplesCount++;
+        }
+    }
+
+    // Add all multiples in multiples[]
+    for (int i = 0; i < multiplesCount; i++){
+        multiplesTotal += multiples[i];
+    }
 
 
 
     // OUTPUT
 //    printf("%s\n\n", name);
 //    printf("%i\n", courseNumber);
-//    printf("%s\n", courseTitle);
 //    printf("C Project %i\n\n", projectNumber);
-//    printf("%s\n\n", currentDate);
 
+    printf("List of multiples: ");
+    for (int i = 0; i < multiplesCount; i++){
+        printf("%i, " , multiples[i]);
+    }
+    printf("\n");
+
+    printf("Count: %i\n", multiplesTotal);
 }
